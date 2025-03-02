57333f3949df777fc903940addf29b315c290dd8
Saiful Islam <2001025@iot.bdu.ac.bd>
2025-03-03 03:15:57 +0600

    goriber go
    first go program run

---
+++ 1.variable.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, World!")
+	a := 10
+	fmt.Println(a)
+	a = 20
+	fmt.Println(a)
+	var b int = 30
+	fmt.Println(b)
+	b = 40
+	fmt.Println(b)
+	var c, d int = 50, 60
+	fmt.Println(c, d)
+	var x = 70
+	fmt.Println(x)
+	x = 800
+	fmt.Println(x)
+	x = 80
+	fmt.Println(x)
+	const y = 90
+	fmt.Println(y)
+	//y = 100
+	fmt.Println(y)
+	const z int = 110
+	fmt.Println(z)
+	const k = 120
+	fmt.Println(k)
+	const m int = 130
+	fmt.Println(m)
+
+}
