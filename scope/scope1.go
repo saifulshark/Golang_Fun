8ee49a07c984073181caa4417ad2cb229a887fcd
Saiful Islam <2001025@iot.bdu.ac.bd>
2025-05-16 23:55:08 +0600

    scope
    scope starting

---
+++ scope/scope1.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+var a = 1
+var b = 2
+
+func sum(x int, y int) int {
+	z := x + y
+	return z
+}
+func main() {
+	var k = 3
+
+	fmt.Println("Hello, World!")
+	fmt.Println(a, b, k)
+	fmt.Println(sum(a, b))
+	fmt.Println(sum(a, k))
+	//fmt.Println(z)//
+}
